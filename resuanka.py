144c5ce20e29c312ee1c4e19c33ddaaf92376086
R9950X_Sudoer <hhj@sudoer.cn>
2025-09-15 23:05:14 +0800

    更新安价统计

--- resuanka.py
+++ resuanka.py
@@ -26,10 +26,24 @@ def main():
     tid = 41989465
     start_lou = 33540
     ignore_author_user = [62906407]  # 忽略作者本人的帖子
-    keyword = "安价"  # 关键词过滤，不包括关键词的楼层不统计，None为不过滤
+    keyword = None  # 关键词过滤，不包括关键词的楼层不统计，None为不过滤，鉴于有人安价不带安价这两个字，还是手工筛选吧
     endtime = (
         "2025-09-16 18:00"  # 结束时间，格式为"YYYY-MM-DD HH-MM-SS"，None为不限制时间
     )
+
+    not_anjia_lou_list = [
+        33586,
+        33567,
+        33590,
+        33593,
+        33582,
+        33585,
+        33589,
+        33597,
+        33615,
+        33621,
+        33622,
+    ]
 
     client = NGAClient.NGAClient()
 
@@ -50,25 +64,58 @@ def main():
     for page_json in page_jsons:
         content_list.extend(page_json["result"])
 
-    print(len(content_list))
+    if len(content_list) != content_list[-1]["lou"] - start_lou + 1:
+        print("Warning: 楼层数与实际帖子数不符，可能存在吞楼")
+        offset = 0
+        i = start_lou
+        while i <= content_list[-1]["lou"]:
+            if content_list[i - start_lou]["lou"] - offset != i:
+                print(f"缺少楼层 {i}")
+                offset += 1
+            i += 1
 
     anjia_list = []
+    ignored_anjia_list = []
     # 元素为{ "authorid": int,"authorname":str, "content": list[(int,str)]}
     # 其中content为该作者的所有安价内容，int为楼层，str为内容
     for content in content_list:
-        if content["author"]["uid"] in ignore_author_user:
-            continue
+        this_anjia = {
+            "authorid": content["author"]["uid"],
+            "authorname": content["author"]["username"],
+            "content": [(content["lou"], content["content"])],
+        }
 
-        if endtime is None or content["postdate"] > endtime:
+        ignore_cond = [
+            (
+                content["lou"] <= start_lou,
+                f"楼层 {content['lou']} 小于等于起始楼层 {start_lou}",
+            ),
+            (
+                content["author"]["uid"] in ignore_author_user,
+                f"作者{content['author']['username']}在忽略名单中",
+            ),
+            (
+                endtime is not None and content["postdate"] > endtime,
+                f"时间 {content['postdate']} 超过截止时间 {endtime}",
+            ),
+            (
+                content["lou"] in not_anjia_lou_list,
+                f"楼层 {content['lou']} 在忽略楼层名单中",
+            ),
+        ]
+
+        ignore_this = False
+        for cond, reason in ignore_cond:
+            if cond:
+                print(f"忽略楼层 {content['lou']}，原因：{reason}")
+                this_anjia["ignore_reason"] = reason
+                ignore_this = True
+                break
+        if ignore_this:
+            ignored_anjia_list.append(this_anjia)
             continue
 
         if keyword is None or keyword in content["content"]:
-
-            this_anjia = {
-                "authorid": content["author"]["uid"],
-                "authorname": content["author"]["username"],
-                "content": [(content["lou"], content["content"])],
-            }
 
             have_same_author_anjia = False
             # 查找前面有没有同一作者的安价
@@ -77,12 +124,19 @@ def main():
                     # 找到同一作者的安价，合并内容
                     anjia_list[i]["content"].extend(this_anjia["content"])
                     have_same_author_anjia = True
+                    print(
+                        f"注意有同作者的重复安价请人工检查: {this_anjia['content'][0]}"
+                    )
                     break
 
             if not have_same_author_anjia:
                 anjia_list.append(this_anjia)
 
     print(f"总安价数: {len(anjia_list)}")
+    print(f"总忽略楼层数: {len(ignored_anjia_list)}")
+    print(f"总楼层数: {len(content_list)}")
+    if len(anjia_list) + len(ignored_anjia_list) != len(content_list):
+        print("Warning: 安价数与总楼层数不符，请检查忽略名单是否正确")
 
     # 保存结果到安价.xlsx
 
@@ -119,13 +173,44 @@ def main():
                 ignore_index=True,
             )
 
+    df_ignored = pd.DataFrame(
+        columns=[
+            "作者ID",
+            "作者名称",
+            "楼层",
+            "内容",
+            "忽略原因",
+        ]
+    )
+
+    for ignored_anjia in ignored_anjia_list:
+        for i in range(len(ignored_anjia["content"])):
+            assert i == 0
+            lou, content = ignored_anjia["content"][i]
+            content = html_and_bbcode_cleaner(content)
+            df_ignored = pd.concat(
+                [
+                    df_ignored,
+                    pd.DataFrame(
+                        {
+                            "作者ID": [ignored_anjia["authorid"]],
+                            "作者名称": [ignored_anjia["authorname"]],
+                            "楼层": [lou],
+                            "内容": [content],
+                            "忽略原因": [ignored_anjia.get("ignore_reason", "")],
+                        }
+                    ),
+                ],
+                ignore_index=True,
+            )
+
     with pd.ExcelWriter("安价.xlsx", engine="xlsxwriter") as writer:
         df.to_excel(writer, sheet_name="Sheet1", index=False)
-
+        wrap_format = writer.book.add_format({"text_wrap": True})
         # 设置列宽
         worksheet = writer.sheets["Sheet1"]
         worksheet.set_column("B:B", 10)
-        worksheet.set_column("C:C", 15)
+        worksheet.set_column("C:C", 15, wrap_format)
         worksheet.set_column("E:E", 100)
         worksheet.set_column("F:F", 20)
 
@@ -138,8 +223,16 @@ def main():
 
             worksheet.set_row(i + 1, estimated_lines * 25)
 
-        wrap_format = writer.book.add_format({"text_wrap": True})
         worksheet.set_column("E:E", 100, wrap_format)
+
+        df_ignored.to_excel(writer, sheet_name="Ignored", index=False)
+        worksheet_ignored = writer.sheets["Ignored"]
+        worksheet_ignored.set_column("B:B", 10)
+        worksheet_ignored.set_column("D:D", 100, wrap_format)
+        for i in range(len(df_ignored)):
+            content_length = len(df_ignored.at[i, "内容"])
+            estimated_lines = content_length // 100 + 1
+            worksheet_ignored.set_row(i + 1, estimated_lines * 25)
 
 
 if __name__ == "__main__":
